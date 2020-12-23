45f431feb31d9d6064ecc41c9946dffa558ae861
Danil Ineev <sai@notreally.media>
2020-12-23 10:18:44 +0000

    Group media

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 import os
 import re
 import tempfile
@@ -6,7 +7,7 @@ from urllib.parse import urlparse
 
 import instaloader
 import requests
-from telegram import MessageEntity
+from telegram import MessageEntity, InputMediaPhoto, InputMediaVideo
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
@@ -16,7 +17,8 @@ INSTALOADER = instaloader.Instaloader()
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-def _download_and_reply(update, post, filename):
+def _download(post, filename):
+    logging.debug(f"Downloading {post}")
     with tempfile.TemporaryDirectory() as dirname:
         prefix = ''
         if post.is_video:
@@ -26,13 +28,14 @@ def _download_and_reply(update, post, filename):
 
         url = getattr(post, f'{prefix}url')
         INSTALOADER.download_pic(f'{dirname}/{filename}', url, datetime.datetime.now())
+        logging.debug("Post has been downloaded")
         urlmatch = re.search('\\.[a-z0-9]*\\?', url)
         file_extension = url[-3:] if urlmatch is None else urlmatch.group(0)[1:-1]
         with open(f'{dirname}/{filename}.{file_extension}', 'rb') as f:
             if post.is_video:
-                update.message.reply_video(f)
+                return InputMediaVideo(f)
             else:
-                update.message.reply_photo(f)
+                return InputMediaPhoto(f)
 
 def download_url(update, context):
     parsed_url = urlparse(update.message.text.strip())
@@ -40,18 +43,23 @@ def download_url(update, context):
     post = instaloader.Post.from_shortcode(INSTALOADER.context, shortcode)
 
     download_original = True
+    media_list = []
     for i, node in enumerate(post.get_sidecar_nodes()):
         download_original = False
-        _download_and_reply(update, node, i)
+        media_list.append(_download(node, i))
     
-    if not download_original:
-        return
+    if download_original:
+        media_list.append(_download(post, 'post'))
 
-    _download_and_reply(update, post, 'post')
-
+    logging.debug("Media downloaded, sending")
+    update.message.reply_media_group(media_list)
+    logging.debug("Media sent")
 
 def main():
     token = os.environ.get('TELEGRAM_BOT_TOKEN')
+    
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
     updater = Updater(token=token, use_context=True)
     dispatcher = updater.dispatcher
     start_handler = CommandHandler('start', start)
